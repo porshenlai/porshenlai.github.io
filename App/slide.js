4d05a398f3f3cde41cfb14a5a56b5d77b21c9c76
Porshen Lai <porshenlai@gmail.com>
2025-11-26 13:09:02 +0800

    20251126

--- App/slide.js
+++ App/slide.js
@@ -187,7 +187,7 @@ class Quiz
 			if (SE) return;
 			SE=document.createElement("style");
 			SE.innerHTML=`
-[qi] { border:2px solid blue;padding:1px 4px;margin:4px 1px; }
+[qi] { border:2px solid blue;border-radius:8px;padding:8px;margin:8px 4px; }
 [qo] { padding:4px; margin:4px; border:2px solid lightgrey;background-image: linear-gradient(white 60%,lightgrey); }
 [qo]:hover { border-color:grey;background-image: linear-gradient(white 60%,grey); }
 [qt][qr="x"] { border-color:red;background-image: linear-gradient(to right,white 60%,pink); }
@@ -203,7 +203,11 @@ class Quiz
 		this.E=e;
 		this.AnsDB=ans||((ans)=>{
 			//document.head.setAttribute("AID",btoa(JSON.stringify({ "1":3, "2-1":2, "3":2, "4":7 })));
-			if (!ans) return {};
+			if (!ans)
+				return Array.from(this.E.querySelectorAll('[qi][___]')).reduce((r,e)=>{
+					r[e.getAttribute('qi')]=parseInt(e.getAttribute('___'));
+					return r;
+				},{});
 			return JSON.parse(ans.getAttribute("ANS")||atob(ans.getAttribute("AID")));
 		})(document.body.querySelector("[ANS]")||document.body.querySelector("[AID]")||document.head.querySelector("[AID]"));
 		console.log(btoa(JSON.stringify(this.AnsDB)));
