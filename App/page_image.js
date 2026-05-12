52f8cfad04760dfaf3aefdca6dbf5900640fb685
Porshen Lai <porshenlai@gmail.com>
2026-05-12 22:26:55 +0800

    20260512

--- App/page_image.js
+++ App/page_image.js
@@ -1,12 +1,16 @@
 (function(SCRIPT){
 
+const MENU=`
+	<div class='full hide mask' style='position:absolute;background:rgba(127,127,127,0.5);'>MENU COME HERE</div>
+`;
+
 function fillContent(elem, url, type) {
 	switch (type) {
 	default: case "photo":
-		elem.innerHTML=`<div style='overflow:hidden;display:flex;justify-content:center;align-items:center;height:100%;'><img src='${url}' style='object-fit:contain;width:100%;height:100%;'/></div>`;
+		elem.innerHTML=`<div style='overflow:hidden;display:flex;justify-content:center;align-items:center;height:100%;'><img src='${url}' style='object-fit:contain;width:100%;height:100%;'/>${MENU}</div>`;
 		break;
 	case "image":
-		elem.innerHTML=`<div style='overflow:hidden;height:100%;'><img src='${url}' style='object-fit:cover;width:auto;height:auto;'/></div>`;
+		elem.innerHTML=`<div style='overflow:hidden;height:100%;'><img src='${url}' style='object-fit:cover;width:auto;height:auto;'/>${MENU}</div>`;
 		((img)=>{
 			img.addEventListener('load',()=>{
 				const cr = DE.getBoundingClientRect();
@@ -19,7 +23,7 @@ function fillContent(elem, url, type) {
 		})(elem.querySelector('img'));
 		break;
 	case "object":
-		elem.innerHTML=`<div style='overflow:hidden;display:flex;justify-content:center;align-items:center;height:100%;'><iframe src='${url}' style='width:100%;height:100%;'><a href='${url}'>Not support, download to open</a></iframe></div>`;
+		elem.innerHTML=`<div style='overflow:hidden;display:flex;justify-content:center;align-items:center;height:100%;'><iframe src='${url}' style='width:100%;height:100%;'><a href='${url}'>Not support, download to open</a></iframe>${MENU}</div>`;
 		break;
 	}
 	elem.classList.add('resolved');
@@ -30,6 +34,16 @@ SCRIPT.value=async function (slide, elem, code) {
 	if (elem.classList.contains('resolved')) return;
 	if(!code) code=elem.innerHTML;
 	fillContent(elem,code,'photo');
+	elem.firstChild.addEventListener("click",(evt)=>{
+		let mask=elem.querySelector('.mask');
+		if (mask.classList.contains('hide')) {
+			let lts=(mask.lts||0),cts=new Date().getTime();
+			console.log(lts,cts,cts-lts);
+			if (cts-lts<500) {
+				mask.classList.remove('hide');
+			} else mask.lts=cts;
+		} else mask.classList.add('hide');
+	});
 };
 
 })(document.currentScript);
