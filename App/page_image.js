b964ee06fac5aacdcf033c88443bd11074668d62
Porshen Lai <porshenlai@gmail.com>
2026-05-15 12:00:31 +0800

    20260515

--- App/page_image.js
+++ App/page_image.js
@@ -1,5 +1,6 @@
 (function(SCRIPT){
 
+// TO REMOVE {{{
 const MENU=`
 	<div class='full hide mask' style='position:absolute;background:rgba(127,127,127,0.5);'>MENU COME HERE</div>
 `;
@@ -27,23 +28,52 @@ function fillContent(elem, url, type) {
 		break;
 	}
 	elem.classList.add('resolved');
-}
-
+}	// }}}
 
 SCRIPT.value=async function (slide, elem, code) {
 	if (elem.classList.contains('resolved')) return;
-	if(!code) code=elem.innerHTML;
-	fillContent(elem,code,'photo');
-	elem.firstChild.addEventListener("click",(evt)=>{
-		let mask=elem.querySelector('.mask');
-		if (mask.classList.contains('hide')) {
-			let lts=(mask.lts||0),cts=new Date().getTime();
-			console.log(lts,cts,cts-lts);
-			if (cts-lts<500) {
-				mask.classList.remove('hide');
-			} else mask.lts=cts;
-		} else mask.classList.add('hide');
-	});
+	if (!code){
+		if (elem.querySelector('img')) return;
+		code=elem.innerHTML;
+	}
+	code=code.split(/[;,\n]/);
+	//fillContent(elem,code,'photo');
+
+	elem.innerHTML=`<div style='overflow:hidden;height:100%;'>
+	<img src='${code[0]}' style='object-fit:cover;width:auto;height:auto;'/>
+	<div class='full hide mask' style='position:absolute;background:rgba(127,127,127,0.5);'>MENU COME HERE</div>
+</div>`;
+	((img)=>{
+		const container=img.parentNode;
+		img.addEventListener('load',()=>{
+			const cr = container.getBoundingClientRect();
+			const as = (img.width*cr.height > img.height*cr.width)
+				? ['height','overflow-x']
+				: ['width','overflow-y'] ;
+ 			img.style[as[0]]='100%';
+			elem.style[as[1]]='auto';
+		});
+		container.addEventListener("click",(evt)=>{
+			for (let e=evt.target;e!==container;e=e.parentNode) if (e.dataset.h) {
+				switch(e.dataset.h){
+				default:
+					console.log("Trigger:",e.dataset.h);
+					break;
+				}
+				evt.stopPropagation();
+				evt.preventDefault();
+				return;
+			}
+			let mask=elem.querySelector('.mask');
+			if (mask.classList.contains('hide')) {
+				let lts=(mask.lts||0),cts=new Date().getTime();
+				console.log(lts,cts,cts-lts);
+				if (cts-lts<500) {
+					mask.classList.remove('hide');
+				} else mask.lts=cts;
+			} else mask.classList.add('hide');
+		});
+	})(elem.querySelector('img'));
 };
 
 })(document.currentScript);
