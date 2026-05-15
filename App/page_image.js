f869ef29f41631c3eff3cbec4d029083e832f621
Porshen Lai <porshenlai@gmail.com>
2026-05-16 02:33:19 +0800

    20260515

--- App/page_image.js
+++ App/page_image.js
@@ -30,6 +30,10 @@ function fillContent(elem, url, type) {
 	elem.classList.add('resolved');
 }	// }}}
 
+function fp2(v) {
+	return Math.floor(v*100)/100;
+}
+
 class PlayList {
 	constructor (urls, canvas) {
 		this.Rs=(Array.isArray(urls) ? urls : urls.split(/[;,\n]/)).map((u)=>({"U":u}));
@@ -39,13 +43,10 @@ class PlayList {
 			const container = canvas.parentNode, cr = container.getBoundingClientRect();
 			const rec = this.Rs[this.C];
 			[rec.O,rec.R] = canvas.width*cr.height > canvas.height*cr.width ?
-				['width',100*cr.height*canvas.width/canvas.height/cr.width] :
-				['height',100*cr.width*canvas.height/canvas.width/cr.height] ;
-			canvas.style[rec.O]="100%";
-			canvas.style[rec.O]=rec.R+"%";
-			console.log(rec.R);
-			canvas.style[rec.O==='width'?'height':'width']='auto';
-			// container.style.overflow="";
+				[true, fp2(cr.height*canvas.width/canvas.height/cr.width-0.03)] :
+				[false, fp2(cr.width*canvas.height/canvas.width/cr.height-0.03)] ;
+			this.scale(1);
+			//this.scale(rec.R);
 		});
 		this.go(0);
 	}
@@ -53,21 +54,61 @@ class PlayList {
 		this.C=i;
 		this.I.src=this.Rs[i].U;
 	}
+	scale (v) {
+		const container = this.I.parentNode, cr = container.getBoundingClientRect();
+		const rec = this.Rs[this.C];
+		switch (v) {
+		case 'contain': v=1.0; break;
+		case 'cover': v=rec.R; break;
+		}
+		if (rec.O) {
+			this.I.style.width=(v*100)+"%";
+			this.I.style.height='auto';
+			container.style.overflow = v>rec.R ? 'auto' : v>1 ? 'auto hidden' : 'hidden';
+			container.style.flexFlow = 'column nowrap';
+		} else {
+			this.I.style.height=(v*100)+"%";
+			this.I.style.width='auto';
+			container.style.overflow = v>rec.R ? 'auto' : v>1 ? 'hidden auto' : 'hidden';
+			container.style.flexFlow = 'row nowrap';
+		}
+		return v;
+	}
 }
 
 SCRIPT.value=async function (slide, elem, code) {
 	if (elem.classList.contains('resolved')) return;
 	elem.classList.add('resolved');
 	if (!code) code=elem.innerHTML;
-	elem.innerHTML="<div class='full centerBox'><img/><div class='full hide mask' style='position:absolute;background:rgba(127,127,127,0.5);'>MENU COME HERE</div></div>";
+	elem.innerHTML=`<div class='full centerBox'><img style='margin:auto;'/><div class='full hide mask' style='position:absolute;background:rgba(127,127,127,0.5);'>
+	<div data-h='scale' style='background:white;'>
+		<input type='range' min='0.5' max='2' step='0.1'></input>
+		<div>
+			<button data-h='scaleContain'>Contain</button>
+			<button data-h='scaleCover'>Cover</button>
+			<output type='number'></output>
+		</div>
+	</div>
+</div></div>`;
 	elem.PlayList=new PlayList(code, elem.querySelector('img'));
+	const Scaler=new (class {
+		constructor (es) { this.ES=es; this.set(1); }
+		get () { return this.ES[0].value; }
+		set (v) {
+			v=elem.PlayList.scale(v);
+			for (let e of this.ES) e.value=v;
+		}
+	})([
+		elem.querySelector('input[type="range"]'),
+		elem.querySelector('output')
+	]);
 	const container=elem.PlayList.I.parentNode;
 	container.addEventListener("click",(evt)=>{
 		for (let e=evt.target;e!==container;e=e.parentNode) if (e.dataset.h) {
 			switch(e.dataset.h){
-			default:
-				console.log("Trigger:",e.dataset.h);
-				break;
+			case 'scale': Scaler.set(Scaler.get()); break;
+			case 'scaleContain': Scaler.set('contain'); break;
+			case 'scaleCover': Scaler.set('cover'); break;
 			}
 			evt.stopPropagation();
 			evt.preventDefault();
@@ -76,10 +117,7 @@ SCRIPT.value=async function (slide, elem, code) {
 		let mask=elem.querySelector('.mask');
 		if (mask.classList.contains('hide')) {
 			let lts=(mask.lts||0),cts=new Date().getTime();
-			console.log(lts,cts,cts-lts);
-			if (cts-lts<500) {
-				mask.classList.remove('hide');
-			} else mask.lts=cts;
+			if (cts-lts<500) mask.classList.remove('hide'); else mask.lts=cts;
 		} else mask.classList.add('hide');
 	});
 };
